49c47184c96d1b1ae1bab6656c57d1e8c1f02141
kumar saurabh <kumarsaurabh27d@gmail.com>
2025-01-16 16:26:29 +0530

    added url

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 // Define the MongoDB connection URL
-const mongoURL = process.env.MONGODB_URL_LOCAL // Replace 'mydatabase' with your database name
+const mongoURL = "mongodb+srv://ankitapprajapati6268:Wo6kl5iq6rPeWGW2@cluster0.2hxg0.mongodb.net/"
 // const mongoURL = process.env.MONGODB_URL;
 
 // Set up MongoDB connection
